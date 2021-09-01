88b92c5346d6354c5d2da101b568b2345cbe984d
okstring <milkoi7801@gmail.com>
2021-09-01 12:35:14 +0900

    feat: storage 부분 구성

---
+++ RxSidedish/Service/SidedishStorage.swift
@@ -0,0 +1,28 @@
+//
+//  SidedishStorage.swift
+//  RxSidedish
+//
+//  Created by Issac on 2021/09/01.
+//
+
+import Foundation
+import RxSwift
+
+class SidedishStorage: SidedishStorageType {
+    private var sidedishes = [[SidedishItem]]()
+    private var currentSidedish = SidedishItem.EMPTY
+    
+    private lazy var sections: [SidedishSectionModel] = [
+        SidedishSectionModel(model: 0, items: sidedishes[0]),
+        SidedishSectionModel(model: 1, items: sidedishes[1]),
+        SidedishSectionModel(model: 2, items: sidedishes[2])
+    ]
+    
+    private lazy var store = BehaviorSubject<[[SidedishItem]]>(value: sidedishes)
+    
+    func allUpdateSidedish(of category: SidedishCategory, newSidedishes: [SidedishItem]) -> Observable<([SidedishItem], SidedishCategory)> {
+        sidedishes[category.index] = newSidedishes
+        store.onNext(sidedishes)
+        return Observable.just((newSidedishes, category))
+    }
+}
---
+++ RxSidedish/Service/SidedishStorageType.swift
@@ -0,0 +1,28 @@
+//
+//  SidedishStorageType.swift
+//  RxSidedish
+//
+//  Created by Issac on 2021/09/01.
+//
+
+import Foundation
+import RxSwift
+
+enum SidedishCategory {
+    case main(title: String, sidedishes: [SidedishItem])
+    case soup(title: String, sidedishes: [SidedishItem])
+    case side(title: String, sidedishes: [SidedishItem])
+    
+    var index: Int {
+        switch self {
+        case .main: return 1
+        case .soup: return 2
+        case .side: return 3
+        }
+    }
+}
+
+protocol SidedishStorageType {
+    @discardableResult
+    func allUpdateSidedish(of category: SidedishCategory, newSidedishes: [SidedishItem]) -> Observable<([SidedishItem], SidedishCategory)>
+}
