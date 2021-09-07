9dad9a627d3e86686898e7f0713af45ff9680289
okstring <milkoi7801@gmail.com>
2021-09-07 23:41:22 +0900

    fix: DetailSidedishItem DTO 세부사항 수정

---
+++ RxSidedish/Entity/DetailBody.swift
@@ -0,0 +1,12 @@
+//
+//  DetailBody.swift
+//  RxSidedish
+//
+//  Created by Issac on 2021/09/07.
+//
+
+import Foundation
+
+struct DetailBody: Decodable {
+    var data: DetailSidedishItem
+}
--- RxSidedish/Entity/DetailSidedishItem.swift
+++ RxSidedish/Entity/DetailSidedishItem.swift
@@ -7,21 +7,27 @@
 
 import Foundation
 
-struct DetailSidedishItem: Decodable {
+struct DetailSidedishItem: Decodable, Equatable {
     let topImageURL: String
-    let thumbImagesURL: [String]
+    let subTopImages: [String]
+    var thumbnailImagesURL: [String] {
+        return [topImageURL] + subTopImages
+    }
     let productDescription: String
     let point: String
     let deliveryInfo: String
     let deliveryFee: String
     let prices: [String]
+    var price: String? {
+        return prices.last
+    }
     let detailSectionImagesURL: [String]
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let bodyContainer = try container.nestedContainer(keyedBy: BodyCodingKeys.self, forKey: .data)
         self.topImageURL = try bodyContainer.decode(String.self, forKey: .topImageURL)
-        self.thumbImagesURL = try bodyContainer.decode([String].self, forKey: .thumbImagesURL)
+        self.subTopImages = try bodyContainer.decode([String].self, forKey: .subTopImagesURL)
         self.productDescription = try bodyContainer.decode(String.self, forKey: .productDescription)
         self.point = try bodyContainer.decode(String.self, forKey: .point)
         self.deliveryInfo = try bodyContainer.decode(String.self, forKey: .deliveryInfo)
@@ -37,10 +43,27 @@ struct DetailSidedishItem: Decodable {
     enum BodyCodingKeys: String, CodingKey {
         case point, prices
         case topImageURL = "top_image"
-        case thumbImagesURL = "thumb_images"
+        case subTopImagesURL = "thumb_images"
         case productDescription = "product_description"
         case deliveryInfo = "delivery_info"
         case deliveryFee = "delivery_fee"
         case detailSectionImagesURL = "detail_section"
+    }
+    
+    init(topImageURL: String, subTopImages: [String], productDescription: String, point: String, deliveryInfo: String, deliveryFee: String, prices: [String], detailSectionImagesURL: [String]) {
+        self.topImageURL = topImageURL
+        self.subTopImages = subTopImages
+        self.productDescription = productDescription
+        self.point = point
+        self.deliveryInfo = deliveryInfo
+        self.deliveryFee = deliveryFee
+        self.prices = prices
+        self.detailSectionImagesURL = detailSectionImagesURL
+    }
+}
+
+extension DetailSidedishItem {
+    static var EMPTY: DetailSidedishItem {
+        return DetailSidedishItem(topImageURL: "", subTopImages: [String](), productDescription: "", point: "", deliveryInfo: "", deliveryFee: "", prices: [String](), detailSectionImagesURL: [String]())
     }
 }
