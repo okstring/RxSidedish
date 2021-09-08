062e65f18dc959a4f0479ba75ef79f5821b16312
okstring <milkoi7801@gmail.com>
2021-09-09 01:41:32 +0900

    feat: ThumbnailStackView image 추가하는 메소드 구현

--- RxSidedish/View/ThumbnailStackView.swift
+++ RxSidedish/View/ThumbnailStackView.swift
@@ -8,4 +8,22 @@
 import UIKit
 
 class ThumbnailStackView: UIStackView {
+    
+    func addArrangedImageView(image: UIImage?, width: CGFloat?) {
+        guard let width = width else {
+            return
+        }
+        
+        let resizeImage = image?.resize(newWidth: width)
+        let imageView = UIImageView(image: resizeImage)
+        imageView.contentMode = .scaleAspectFill
+        
+        addArrangedSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: width),
+            imageView.heightAnchor.constraint(equalToConstant: width / 5 * 4)
+        ])
+    }
 }
