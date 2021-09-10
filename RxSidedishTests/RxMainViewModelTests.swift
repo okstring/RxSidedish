bc326159fea419ff0e8be461c0477bf6cf36d1e5
okstring <milkoi7801@gmail.com>
2021-09-10 23:23:52 +0900

    test: RxMainViewModelTests 구현

---
+++ RxSidedishTests/RxMainViewModelTests.swift
@@ -0,0 +1,44 @@
+//
+//  RxMainViewModelTests.swift
+//  RxSidedishTests
+//
+//  Created by Issac on 2021/09/10.
+//
+
+import XCTest
+@testable import RxSidedish
+
+import RxSwift
+import RxBlocking
+
+class RxMainViewModelTests: XCTestCase {
+    
+    var viewModel: MainViewModel!
+    var sceneCoordinator: SceneCoordinatorType!
+    var storage: SidedishStorage!
+    var networkUseCase: NetworkUseCase!
+    
+    override func setUp() {
+        
+        let VC = UIViewController()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = VC
+        
+        sceneCoordinator = SceneCoordinator(window: window)
+        storage = SidedishStorage()
+        networkUseCase = NetworkUseCase()
+        
+        viewModel = MainViewModel(title: "",
+                                  sceneCoordinator: sceneCoordinator,
+                                  storage: storage,
+                                  networkUseCase: networkUseCase)
+    }
+    
+    func test_SidedishesFetch() {
+        viewModel.fetchItems.onNext(())
+        
+        let fetched = try! viewModel.storage.sidedishesList().toBlocking().first()!
+        
+        XCTAssertEqual(fetched.count == 3, true)
+    }
+}
